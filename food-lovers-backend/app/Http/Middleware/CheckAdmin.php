439552a162c2819955b8d00ed69cc27a454ec87a
Khadija2104 <khadijakhalid.2104@gmail.com>
2026-01-17 21:00:42 +0400

    Create CheckAdmin.php

---
+++ food-lovers-backend/app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckAdmin
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->user() || $request->user()->role !== 'admin') {
+            return response()->json(['success' => false, 'message' => 'Unauthorized'], 403);
+        }
+
+        return $next($request);
+    }
+}
